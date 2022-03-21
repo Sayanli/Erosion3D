013fb28e55a454bde0f781d611c26dc38bd487f0
Sayanli <ily_kovrov@mail.ru>
2022-03-21 20:24:42 +0300

    new submodule add/imgui demo

--- Engine/src/test.cpp
+++ Engine/src/test.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <imgui/imgui.h>
+#include <imgui/backends/imgui_impl_opengl3.h>
 
 #include "test.h"
 
@@ -9,6 +11,7 @@ namespace Engine {
 	{
 		std::cout << "Engine start" << std::endl;
 
+
         GLFWwindow* window;
 
         /* Initialize the library */
@@ -16,7 +19,7 @@ namespace Engine {
             return -1;
 
         /* Create a windowed mode window and its OpenGL context */
-        window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+        window = glfwCreateWindow(1024, 768, "Tech demo", NULL, NULL);
         if (!window)
         {
             glfwTerminate();
@@ -32,14 +35,44 @@ namespace Engine {
             return -1;
         }
 
-        glClearColor(255, 0, 0, 0);
+        std::string vendor = "Vendror: ";
+        vendor += (const char*)glGetString(GL_VENDOR);
+        std::string renderer = "Renderer: ";
+        renderer += (const char*)glGetString(GL_RENDERER);
+        std::string version = "Version: ";
+        version += (const char*)glGetString(GL_VERSION);
+        std::string glsl_version = "GLSL Version: ";
+        glsl_version += (const char*)glGetString(GL_SHADING_LANGUAGE_VERSION);
 
+
+        std::cout << vendor << std::endl;
+        std::cout << renderer << std::endl;
+        std::cout << version << std::endl;
+        std::cout << glsl_version << std::endl;
+
+        glClearColor(0, 0, 1, 0);
+
+        IMGUI_CHECKVERSION();
+        ImGui::CreateContext();
+        ImGui_ImplOpenGL3_Init();
 
         /* Loop until the user closes the window */
         while (!glfwWindowShouldClose(window))
         {
             /* Render here */
             glClear(GL_COLOR_BUFFER_BIT);
+
+
+            ImGuiIO& io = ImGui::GetIO();
+            io.DisplaySize.x = static_cast<float>(1024);
+            io.DisplaySize.y = static_cast<float>(768);
+
+            ImGui_ImplOpenGL3_NewFrame();
+            ImGui::NewFrame();
+
+            ImGui::ShowDemoWindow();
+            ImGui::Render();
+            ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
             /* Swap front and back buffers */
             glfwSwapBuffers(window);
